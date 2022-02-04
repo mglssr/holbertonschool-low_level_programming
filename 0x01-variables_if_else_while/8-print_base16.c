b744ed461ffc94bd1f3162cf872059aa4385bc33
mglssr <iglesiasmaia6@gmail.com>
2022-02-04 12:41:24 -0800

    commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main- Return Point
+*
+*Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+		int n;
+
+		for (n = 0; n < 10; n++)
+			{
+		putchar(n);
+			}
+			for (n = 'a'; n <= 'f'; n++)
+			{
+				putchar(n);
+			}
+				printf("\n");
+					return (0);
+}
