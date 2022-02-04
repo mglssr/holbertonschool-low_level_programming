5b9da059ad010678bc4375603600a54da6866c54
mglssr <iglesiasmaia6@gmail.com>
2022-02-04 09:29:28 -0800

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+* main- Return Point
+*
+*Return: Always 0 (Success)
+*
+*/
+
+int main(void)
+{
+		char ch, CH;
+
+			for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			putchar(ch);
+		}
+		for (CH = 'A'; CH <= 'Z'; CH++)
+		{
+			putchar(CH);
+		}
+			putchar('\n');
+			return (0);
+}
