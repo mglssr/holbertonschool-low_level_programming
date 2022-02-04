90fb86308bdb92aaf08758c759cdd63511ddb315
mglssr <iglesiasmaia6@gmail.com>
2022-02-04 11:27:00 -0800

    commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main- Return Point
+*
+*Return: Always 0 (Success)
+*
+*/
+int main(void)
+{
+	int n = 48;
+
+		for (; n <= 57; n++)
+		{
+		putchar(n);
+}
+		putchar('\n');
+		return (0);
+}
