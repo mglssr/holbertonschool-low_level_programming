6fd9c8ada72557170220509bdfd7ea5e6ea8a903
mglssr <iglesiasmaia6@gmail.com>
2022-02-04 17:26:17 -0800

    commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int x, d;
+
+	for (x = 48; x <= 57; x++)
+	{
+		for (d = x; d <= 57; d++)
+		{
+			if (x != d)
+			{
+			putchar(x);
+			putchar(d);
+			if (x < 56)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
