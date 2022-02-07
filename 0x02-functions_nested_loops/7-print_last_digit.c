f9414d617b7714c309c211e18061244da59cf375
mglssr <iglesiasmaia6@gmail.com>
2022-02-07 14:42:02 -0800

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include ""main.h"
+/**
+* jack_bauer - sjasa
+* Return: 24 hours
+*/
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; h < 60; m++)
+		{
+		_putchar((h / 10) + '0');
+		_putchar((h % 10) + '0');
+		_putchar(':');
+		_putchar((m / 10) + '0');
+		_putchar((m % 10) + '0');
+		_putchar('\n');
+		}
+		}
+}
