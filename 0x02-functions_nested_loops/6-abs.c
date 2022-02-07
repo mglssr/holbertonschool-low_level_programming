8b4ca338ca398803f57b660505f29775f3edef54
mglssr <iglesiasmaia6@gmail.com>
2022-02-07 14:16:37 -0800

    commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <inttypes.h>
 /**
-* __abs - hehe
-* @ab: abs
+* _abs - hehe
+* ab: abs
 * Return: value ab
 */
 int _abs(int ab)
 {
 	if (ab < 0)
 		ab = ab * -1;
-		return (ab);
+	return (ab);
 }
