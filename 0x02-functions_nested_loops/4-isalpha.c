dd0d858225fbc5e54bf241ea1b7edc72e43897aa
mglssr <iglesiasmaia6@gmail.com>
2022-02-07 13:46:31 -0800

    commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*_isalpha - sioxq
+* @c: aaaa
+*Return: 1 if is a letter, 0 if is otheewise
+*/
+int _isalpha(int c)
+{
+	if (c > 65 && c< 122)
+		return (1);
+
+	else
+		return (0);
+}
