af280908fa7568a52bd723317da5a76948b74ddc
mglssr <iglesiasmaia6@gmail.com>
2022-02-07 10:53:25 -0800

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <math.h>
+#include "main.h"
 /**
  * main - check the code
  *
@@ -9,12 +8,12 @@ void print_alphabet_x10(void)
 
 {
 	int c, count;
-	count = 0;
+count = 0;
 	while (count < 10)
 {
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(c);
+	_putchar(c);
 	}
 	count++;
 	_putchar('\n');
