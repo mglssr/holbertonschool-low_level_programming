1e533b9657ba69915f5986a0d8924ee2c06bb21e
mglssr <iglesiasmaia6@gmail.com>
2022-02-07 16:09:17 -0800

    commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+*
+*
+*
+*/
+void times_table(void)
+{
+	int A, B, res;
+	for (A = 0; A < 10; A++)
+	{
+		for (B = 0; A < 10; B++)
+		{	
+			res = B * A
+			if (B != 0)
+			{
+	_putchar(' ');
+	if (res < 10)
+	_putchar(' ');
+}
+
+if (res > 9)
+{
+	_putchar((res / 10) + '0');
+	_putchar((res % 10) + '0');
+
+else
+	_putchar(res + '0');
+}
+if (B == 9)
+{
+	_putchar('\n');
+
+else
+	_putchar(',');
+}
+}
+}
+}
