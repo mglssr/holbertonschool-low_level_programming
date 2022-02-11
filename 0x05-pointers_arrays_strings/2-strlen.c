e15f6597e4994cf63318e70b9a1e3a1b35ec1276
mglssr <iglesiasmaia6@gmail.com>
2022-02-11 10:23:45 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include <string.h>
+#include "main.h"
+/*
+* int _strlen - función que devuelve el largo de una string
+* @s: puntero s
+* 
+*/
+int _strlen(char *s)
+{
+	_putchar('*s = %zu', strlen(*s));
+}
