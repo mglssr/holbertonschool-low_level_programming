bd13393b360e65911f252816a924d91433701b5d
mglssr <iglesiasmaia6@gmail.com>
2022-02-16 10:49:49 -0800

    commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,18 +8,21 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0, j;
+	int i = 0, j = 0;
 
-	while (dest[i] != '\0')
+	while (dest[i])
 	{
 		i++;
 	}
-
-	for (j = 0; j < n; j++, i++)
+	while (j < n && (src[j] != '\0'))
 	{
-		dest[i] = src[j];
+		dest[j] = src[j];
+		j++;
+	}
+	for (; j < n; j++)
+	{
+		dest[j] = '\0';
 	}
 	return (dest);
 }
-
 
