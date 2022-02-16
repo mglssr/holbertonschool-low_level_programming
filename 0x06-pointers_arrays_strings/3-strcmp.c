a5e50add2ba2e5ded2af9e4c610be32f7a71a51c
mglssr <iglesiasmaia6@gmail.com>
2022-02-16 13:14:03 -0800

    commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* _strcmp - funcion que compara dos strings
+* @s1: string 1
+* @s2: string 2 
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int x;
+	
+	for (x = 0; s1[x] != '\0'; x++)
+	{
+		if (s1[x] != s2[x]);
+		return (s1[x] - s2[x]);
+	}
+	
+	if (s2[x] != '\0')
+	{
+		return (s1[x] - s2[x]);
+	}
+	return (0);
+}
