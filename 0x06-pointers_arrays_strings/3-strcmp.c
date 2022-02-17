e21f7bdfd487abad912dd77b540608f3782783d4
mglssr <iglesiasmaia6@gmail.com>
2022-02-16 17:25:47 -0800

    commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,18 +2,19 @@
 /**
 * _strcmp - funcion que compara dos strings
 * @s1: string 1
-* @s2: string 2 
+* @s2: string 2
+* Return: saaa
 */
 int _strcmp(char *s1, char *s2)
 {
 	int x;
-	
+
 	for (x = 0; s1[x] != '\0'; x++)
 	{
-		if (s1[x] != s2[x]);
+		if (s1[x] != s2[x])
 		return (s1[x] - s2[x]);
 	}
-	
+
 	if (s2[x] != '\0')
 	{
 		return (s1[x] - s2[x]);
