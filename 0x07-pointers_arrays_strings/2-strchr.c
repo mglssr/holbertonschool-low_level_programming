f0778296503783e81b14473808711dc4ceb878a1
mglssr <iglesiasmaia6@gmail.com>
2022-02-21 09:05:18 -0800

    commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,9 +11,12 @@ char *_strchr(char *s, char c)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		s++;
 		if (s[i] == c)
-			return (s + c);
-		else
-			return (0);
+			return (s);
 	}
+	if (c == '\0')
+		return (s);
+
+	return ('\0');
 }
