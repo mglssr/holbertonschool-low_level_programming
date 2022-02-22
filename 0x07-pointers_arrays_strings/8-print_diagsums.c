bd4aeabc0ac4098180b93a14cac364ee3c8fbac4
mglssr <iglesiasmaia6@gmail.com>
2022-02-21 18:36:57 -0800

    commit

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_diagsums - traza de una matriz 
+*@a: dtring
+*@size: tamaño de la matriz
+**/
+void print_diagsums(int *a, int size)
+{
+	 int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i];
+		a += size;
+	}
+
+	a -= size;
+
+	for (i = 0; i < size; i++)
+	{
+		sum2 += a[i];
+		a -= size;
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
