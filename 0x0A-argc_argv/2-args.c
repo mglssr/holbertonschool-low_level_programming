0e809c12d1c826a33252a5f907849524b37f77ae
mglssr <iglesiasmaia6@gmail.com>
2022-02-25 07:50:11 -0800

    commit

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 /**
 * main - qudiq
-* return: sdao
+*@argc: contador idk
+*@argv: aoisdq
+* Return: sdao
 */
 int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	for (i = 0; i < argc; i++)
 	printf("%s\n", argv[i]);
 return (0);
