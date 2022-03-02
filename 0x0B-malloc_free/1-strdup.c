f13b797d963c7971df951924cf5581ad2c47c1d9
mglssr <iglesiasmaia6@gmail.com>
2022-03-02 10:17:50 -0800

    commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* _strlen - soqksoqk
+* @str: string
+* Return: algo idk
+*/
+
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (*str != '\0')
+	{
+		i++;
+		str++;
+	}
+	return (i);
+}
+
+/**
+* _strdup - dwis
+* @str: iajia
+* Return: jsd
+*/
+
+char *_strdup(char *str)
+{
+	char *str2;
+	int i;
+
+	str2 = malloc(_strlen(str) + 1);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		str2[i] = str[i];
+	}
+	str2[i] = '\0';
+	return (str2);
+}
