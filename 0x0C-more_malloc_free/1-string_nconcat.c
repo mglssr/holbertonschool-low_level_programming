79c1bcee052eda848b6260eb125dfd61f446fd39
mglssr <iglesiasmaia6@gmail.com>
2022-03-03 21:07:56 -0800

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,11 +1,9 @@
-#include <stdlib.h>
 #include "main.h"
-#include <string.h>
-#include <stdio.h>
+#include <stdlib.h>
 /**
-*_strlen - returns lenght
-* @str: pointer
-* Return: i
+* _strlen - calculate the lenght
+* @str: string
+* Return: string
 */
 int _strlen(char *str)
 {
@@ -19,39 +17,29 @@ int _strlen(char *str)
 	return (i);
 }
 /**
-*string_nconcat - ida
-* @s1: unu
-* @s2: lloro
-* @n: ahre
-* Return: ahre
+* string_nconcat - a function that concatenates two strings
+* @s1: string one
+* @s2: string two
+* @n: a variable
+* Return: result
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *result;
-	unsigned int id1, id2;
+	char *rslt;
+	unsigned int idx1, idx2;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
 
-	result = malloc(_strlen(s1) + n + 1 * sizeof(*result));
-
-	if (!result)
+	rslt = malloc(_strlen(s1) + n + 1 * sizeof(*rslt));
+	if (!rslt)
 		return (NULL);
-
-	for (id1 = 0; s1[id1]; id1++)
-		result[id1] = s1[id1];
-		
-	printf("coping uwu %d", s1[id1]);
-
-	for (id2 = 0; id2 < n && s2[id2]; id2++)
-		result[id1 + id2] = s2[id2];
-		
-	printf("coping uwu %d", s2[id2]);
-
-	result[id1 + id2] = '\0';
-
-	return (result);
+	for (idx1 = 0; s1[idx1]; idx1++)
+		rslt[idx1] = s1[idx1];
+	for (idx2 = 0; idx2 < n && s2[idx2]; idx2++)
+		rslt[idx1 + idx2] = s2[idx2];
+	rslt[idx1 + idx2] = '\0';
+	return (rslt);
 }
