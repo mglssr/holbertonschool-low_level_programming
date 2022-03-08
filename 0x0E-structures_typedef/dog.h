d83b21ff8ec014988a182019d444ae0dfa6318de
mglssr <iglesiasmaia6@gmail.com>
2022-03-08 14:11:59 -0800

    commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - whd
+* @name: aod
+* @age: ais
+* @owner: isa
+*/
+
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+dog;
+#endif
