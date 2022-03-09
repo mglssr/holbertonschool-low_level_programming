769598562477c33d97ac0b8373a9176afca9661f
mglssr <iglesiasmaia6@gmail.com>
2022-03-09 06:31:46 -0800

    commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+* array_iterator - executes a function given as a parameter on each elementofa
+* @array: array xs
+* @size: tamaño del array
+* @action: aaaaaaa
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
