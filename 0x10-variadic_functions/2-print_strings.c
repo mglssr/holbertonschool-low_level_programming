a3ccd84debdbe16de19140b09b53d482531ad5c3
mglssr <iglesiasmaia6@gmail.com>
2022-03-10 08:14:50 -0800

    commit

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,6 +1,33 @@
 #include "variadic_functions.h"
+#include <stdio.h>
 /**
-* print_strings - 
-*
-*
+* print_strings - funcion que printea una string
+* @separator: sjid
+* @n: aaaaaaa
+* Return: algo idk
 */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ln;
+	char *string;
+
+	va_start(ln, n);
+
+	for (i = 0; i < n;)
+	{
+		string = va_arg(ln, char *);
+		if (!string)
+		printf("nil");
+		else
+	{
+		printf("%s", string);
+		i++;
+	}
+		if (separator != NULL && i < n)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ln);
+}
