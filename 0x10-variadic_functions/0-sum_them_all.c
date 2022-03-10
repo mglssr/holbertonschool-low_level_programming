09360c2f612951a061e4e2225d09631286fb947c
mglssr <iglesiasmaia6@gmail.com>
2022-03-10 04:41:24 -0800

    task 0 owo

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+* int sum_them_all - funcion que retorna la suma todos los parámetros
+* @n: cantidad de parametros
+* Return: algo xd
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ln;
+	unsigned int i;
+	int sum = 0;
+
+	va_start(ln, n);
+		
+		if (n == 0)
+			return (0);
+	
+	for(i = 0; i < n; i++)		
+		sum += va_arg(ln, int);
+	
+	va_end(ln);
+	
+	return (sum);
+}
