2a609f7f9451fa1223e57f9af82912c95249b1c4
mglssr <iglesiasmaia6@gmail.com>
2022-03-11 06:21:17 -0800

    commit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,17 +6,20 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-/**
-* struct pr - Struct pr
-* @ty: el type
-* @f: la func
-*/
+void print_char(va_list ty);
+void print_int(va_list ty);
+void print_float(va_list ty);
+void print_string(va_list ty);
 
+/**
+* struct pr  Struct que voy a usar pa printear idk
+* @ty: el type
+* @f: la function uwu
+*/
 typedef struct pr
 {
   char ty;
   void (*f)(va_list ty);
 } p_rry;
 
-void 
 #endif
