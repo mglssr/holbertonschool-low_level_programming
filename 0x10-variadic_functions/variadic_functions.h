df64048d75fcf0c8a4e40463f6f88e7a60d9d86c
mglssr <iglesiasmaia6@gmail.com>
2022-03-11 19:31:31 -0800

    eeeeeee

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,25 +1,26 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-void print_char(va_list ty);
-void print_int(va_list ty);
-void print_float(va_list ty);
-void print_string(va_list ty);
+void print_char(va_list);
+void print_int(va_list);
+void print_float(va_list);
+void print_string(va_list);
 
 /**
-* struct pr  Struct que voy a usar pa printear idk
-* @ty: el type
-* @f: la function uwu
+* struct pr - con esta struct creo un array que contiene mi type a matchear y su funcion correspondiente
+* @ty: type que busco matchear
+* @f: puntero a va ir a una de las funciones que voy a usar para printear
 */
 typedef struct pr
 {
   char ty;
-  void (*f)(va_list ty);
+  void (*f)(va_list);
 } p_rry;
 
 #endif
