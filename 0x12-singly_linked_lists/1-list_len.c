80cb18460fe17855d743ae5393ec3297eee39b2a
mglssr <iglesiasmaia6@gmail.com>
2022-03-17 11:50:33 -0700

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* list_len - function that returns the number of elements in a linked list
+* @h: puntero uwu
+* Return: number of elements in the linked list
+*/
+
+size_t list_len(const list_t *h)
+{
+	unsigned int count = 0;
+
+	while (h !=  NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
