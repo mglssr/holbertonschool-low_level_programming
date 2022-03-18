1fe1d00ebedcf2ecc2b31c21f3cd6b1f75ad9cd8
mglssr <iglesiasmaia6@gmail.com>
2022-03-17 19:58:56 -0700

    update task 24git push

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,17 +10,18 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new; 
-	
+	list_t *new;
+
 	new = malloc(sizeof(list_t));
 
 	if (new == NULL)
 		return (NULL);
-	
+
 	(*new).str = strdup(str);
 	(*new).len = strlen(str);
+	new->next = *head;
 
-	*head = new->next;
+	*head = new;
 
 return (new);
 }
