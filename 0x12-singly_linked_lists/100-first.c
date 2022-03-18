800d6ca63d68b11297f8c29d12b12c4ba3697f36
mglssr <iglesiasmaia6@gmail.com>
2022-03-18 06:20:56 -0700

    task advanced 1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* b4_main - function that prints something before the main is executed
+*/
+
+void b4_main (void)
+{
+	printf ("You're beat! and yet, you must allow,\n");
+	printf ("I bore my house upon my back!\n");
+}
+
+
+
