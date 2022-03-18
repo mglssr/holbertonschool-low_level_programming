f8ec586d95fecca1505bcc2eb6b450463a1d2b46
mglssr <iglesiasmaia6@gmail.com>
2022-03-18 06:22:16 -0700

    betty la fea

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,11 +5,8 @@
 * b4_main - function that prints something before the main is executed
 */
 
-void b4_main (void)
+void b4_main(void)
 {
-	printf ("You're beat! and yet, you must allow,\n");
-	printf ("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
-
-
-
