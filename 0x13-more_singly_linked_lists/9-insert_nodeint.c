97f819e785100bea8e24eb6e20c3d87dfa1221b6
mglssr <iglesiasmaia6@gmail.com>
2022-03-22 16:59:00 -0700

    task 9 no doy mah

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+
+/**
+* insert_nodeint_at_index - function that insterts a new node at the given pos
+* @head: puntero a puntero uwu
+* @idx: is the index of the list where the new node should be added
+* @n: data(?
+* Return: algo uwu
+*/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *current, *next2;
+
+	current = *head;
+	next2 = malloc(sizeof(listint_t));
+
+	if (next2 == NULL)
+		return (NULL);
+
+	(*next2).n = n;
+
+	if (idx == 0)
+	{
+		(*next2).next = current;
+		*head = next2;
+		return (*head);
+	}
+
+	while (idx > 1)
+	{
+		current = (*current).next;
+		idx--;
+		if (current == NULL)
+		{
+			free(next2);
+			return (NULL);
+		}
+	}
+
+	(*next2).next = (*current).next;
+	(*current).next = next2;
+
+	return (next2);
+}
