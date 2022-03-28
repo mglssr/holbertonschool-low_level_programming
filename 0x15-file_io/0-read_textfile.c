ea9fabd34fc790c4dad4faeb05eb0d643dd09eb3
mglssr <iglesiasmaia6@gmail.com>
2022-03-28 09:43:18 -0700

    add main.h, add mains, add task 0(?)

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+* read_textfile - function that reads a text file and prints it to the stout
+* @filename: archivo a leer
+* @letters: number of letters it should read and print
+* Return: algo idk
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char buff[1024];
+	ssize_t count;
+
+	fd = open(filename,O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+	
+	count = read(fd, buff, letters);
+
+	if (count == -1)
+		return (0);
+
+	write(STDIN_FILENO, buff, count);
+
+	if ((write(STDIN_FILENO, buff, count)) == -1)
+		return (0);
+
+return (count);
+}
