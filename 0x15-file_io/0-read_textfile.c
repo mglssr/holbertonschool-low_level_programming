5ff2e5d963ae2aeeebe8021480e8e339a4af5b8f
mglssr <iglesiasmaia6@gmail.com>
2022-03-28 11:20:26 -0700

    added more frees :D

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,6 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t wr;
 
 	buff = malloc(sizeof(char) * letters);
+	
 	if (buff == NULL)
 		return (0);
 
@@ -26,21 +27,26 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
+	{
+		free(buff);
 		return (0);
+	}
 
 	count = read(fd, buff, letters);
 
 	if (count == -1)
+	{
+		free(buff);
 		return (0);
+	}
 
 	wr = write(STDIN_FILENO, buff, count);
 
 	close(fd);
 
-	if (wr != count && wr == -1)
+	if (wr != count || wr == -1)
 		return (0);
 
 	free(buff);
-
 	return (wr);
 }
