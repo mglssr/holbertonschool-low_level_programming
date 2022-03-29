2d62e957c6305f152ba38a996ea2a74f7df96998
mglssr <iglesiasmaia6@gmail.com>
2022-03-29 06:02:13 -0700

    betty update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,22 +17,22 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	
+
 	if (text_content == NULL)
 		return (1);
-	
+
 	len = strlen(text_content);
 
-	fd = open(filename, O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
 		return (-1);
-	
+
 	wr = write(fd, text_content, len);
 
 	close(fd);
 
-	if (wr == -1)
+	if (wr == -1 || wr != len)
 		return (-1);
 
 return (1);
