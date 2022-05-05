459e496b54cc1f44a5c6b1cfe739801f6869d5d8
mglssr <iglesiasmaia6@gmail.com>
2022-05-05 11:46:02 -0700

    betty in 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,14 +12,14 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new, *aux;
 
 	new = malloc(sizeof(dlistint_t));
-	
-	if(!new)
+
+	if (!new)
 		return (NULL);
 
 	(*new).n = n;
-	
+
 	(*new).next = NULL;
-	
+
 	if (*head == NULL)
 	{
 		(*new).prev = NULL;
@@ -36,5 +36,5 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		(*aux).next = new;
 		(*new).prev = aux;
 	}
-	return(new);
+	return (new);
 }
