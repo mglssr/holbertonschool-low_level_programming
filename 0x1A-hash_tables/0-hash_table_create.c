9cfeecb6205f2548a944c21353b6a0b4cb38bd5f
mglssr <iglesiasmaia6@gmail.com>
2022-05-27 04:29:49 -0700

    make the element of the linked list point to null

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int i = 0;
 
 	if (size == 0)
 		return (NULL);
@@ -21,7 +22,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	(*table).array = calloc(size, sizeof(hash_node_t));
 
 	if (!(*table).array)
+	{
+		free(table);
 		return (NULL);
-
+	}
+	while (i < size)
+	{
+		(*table).array[i] = NULL;
+		i++;
+	}
 	return (table);
 }
