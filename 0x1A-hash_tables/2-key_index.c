768edadbf19929d1a2cdf33886705a802c0326e1
mglssr <iglesiasmaia6@gmail.com>
2022-05-27 04:20:48 -0700

    a

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,8 @@
+#include "hash_tables.h"
+
+/**
+* int key_index - function that gives you the index of a key
+* @key: 
+* @size: 
+* Return: the index at which the key/value pair should be stored in the array
+*/
