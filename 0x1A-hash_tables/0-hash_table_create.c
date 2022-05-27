e47b17222c1086725e9126c65a537dcec9eed8e2
mglssr <iglesiasmaia6@gmail.com>
2022-05-27 04:18:43 -0700

    betty and size == 0 case

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,13 +10,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 
+	if (size == 0)
+		return (NULL);
+
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
 		return (NULL);
-	
+
 	(*table).size = size;
 	(*table).array = calloc(size, sizeof(hash_node_t));
-	
+
 	if (!(*table).array)
 		return (NULL);
 
