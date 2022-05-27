22c74d0fd4f1d6e1fcd36c2efd17306c2e14b5a1
mglssr <iglesiasmaia6@gmail.com>
2022-05-27 07:14:48 -0700

    task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,47 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_set - function that adds an element to the hash table
+* @ht: hash table
+* @key: key
+* @value: the value associated with the key
+* Return: 1 if succeeded or 0 otherwise
+*/
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+unsigned long int index = 0;
+	hash_node_t *searchnode = NULL, *newnode = NULL;
+
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
+
+	index = key_index((unsigned char *)key, (*ht).size);
+
+	newnode = malloc(sizeof(hash_node_t));
+
+	if (newnode == NULL)
+		return (0);
+
+	searchnode = ht->array[index];
+
+	while (searchnode != NULL)
+	{
+		if (strcmp((*searchnode).key, key) == 0)
+		{
+			free((*searchnode).value);
+			(*searchnode).value = strdup(value);
+			free(newnode);
+			return (1);
+		}
+
+		searchnode = (*searchnode).next;
+	}
+
+	(*newnode).key = strdup(key);
+	(*newnode).value = strdup(value);
+	(*newnode).next = (*ht).array[index];
+	(*ht).array[index] = newnode;
+
+	return (1);
+}
