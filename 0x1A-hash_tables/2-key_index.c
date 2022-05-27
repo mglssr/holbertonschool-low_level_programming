7480f97567cee46ca973348aa20213ccacd21762
mglssr <iglesiasmaia6@gmail.com>
2022-05-27 05:31:58 -0700

    task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,8 +1,16 @@
 #include "hash_tables.h"
 
 /**
-* int key_index - function that gives you the index of a key
-* @key: 
-* @size: 
+* key_index - function that gives you the index of a key
+* @key: the key
+* @size: size of the table
 * Return: the index at which the key/value pair should be stored in the array
 */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index = 0;
+
+	index = hash_djb2(key) % size;
+	return (index);
+}
